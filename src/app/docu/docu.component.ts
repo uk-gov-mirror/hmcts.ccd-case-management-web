a164b3ee12b2ef70765a63a5ecb1f45ee3254aa7
Karamath <karamath.mohamed@hmcts.net>
2019-06-06 15:25:22 +0100

    DVK: part 2

--- src/app/docu/docu.component.ts
+++ src/app/docu/docu.component.ts
@@ -2,8 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ccd-docu',
-  templateUrl: './docu.component.html',
-  styleUrls: ['./docu.component.scss']
+  templateUrl: './docu.component.html'
 })
 export class DocuComponent {
   constructor() { }
